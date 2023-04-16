b854f0c73157aa4aaeb3004074e4c32b4654301a
unknown <milkhao0608@gmail.com>
2023-04-17 00:31:42 +0800

    Uploading

---
+++ Gaole_map.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# 爬取網頁HTML內容
+url = "https://pokemongaole.com.tw/stores/"
+response = requests.get(url)
+html_content = response.text
+
+# 使用BeautifulSoup解析HTML內容，提取出<address>標籤內的文本內容
+soup = BeautifulSoup(html_content, "html.parser")
+city_all=soup.find("div", {"class": "city"})
+
+# 取得所有城市
+city_list={}
+for city in city_all.find_all("option"):
+    if "選擇" not in city.get_text():
+        city_list[city.get_text()]=None
+
+for city in city_list:
+    url = "https://pokemongaole.com.tw/stores/" + city
+    response = requests.get(url)
+    html_content = response.text
+
+    # 使用BeautifulSoup解析HTML內容，提取出<address>標籤內的文本內容
+    soup = BeautifulSoup(html_content, "html.parser")
+    district_all=soup.find("div", {"class": "district"})
+
+    #取得所有城市中的區域
+    district_list=[]
+    for district in district_all.find_all("option"):
+        district_list.append(district.get_text())
+    
+    # 把第一筆drop掉
+    district_list=district_list[1:]
+
+    city_list[city]=district_list
+
+# 要處理的地址列表
+address_list = []
+
+for city in city_list:
+    for district in city_list[city]:
+        url = "https://pokemongaole.com.tw/stores/" + city + "/" + district
+        response = requests.get(url)
+        html_content = response.text
+
+        soup = BeautifulSoup(html_content, "html.parser")
+        address_all = soup.find_all("address")
+
+        for address in address_all:
+            address_list.append(address.text.strip()[:-4])
+
+        print(address_list)
+
+df = pd.DataFrame(address_list, columns=["地址"])
+df.to_csv("addresses.csv", encoding="utf_8_sig")
